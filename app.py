43fa644ad2e847830dd17104a090581a8636cb97
priyanahata201 <priya.nahata@hoonartek.com>
2025-06-13 13:54:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import yaml
 import os
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI 
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
